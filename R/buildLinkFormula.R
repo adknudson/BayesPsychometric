4d4518eb95abb8eb913dc9533fe40fe189c22987
Alex Knudson <aknudson@nevada.unr.edu>
2019-10-17 17:26:11 -0700

    fixed bug with intercept only model
    Needed to check if no continuous predictors are specified.

--- R/buildLinkFormula.R
+++ R/buildLinkFormula.R
@@ -11,23 +11,24 @@
 
   # Build a string for the slope terms for each predictor
   m_slope <- NULL
-  for (nv in numeric_vars) {
-    # create the factor coefficients:
-    # bvar1_factor1[factor1] + bvar1_factor2[factor2] + ...
-    if (length(factor_vars) == 0) {
-      factor_coefs <- paste0("b", nv)
-    } else {
-      factor_coefs <- paste0("b", nv, "_",
-                             factor_vars, "[", factor_vars, "]",
-                             collapse = " + ")
-      factor_coefs <- paste0("b", nv, " + ", factor_coefs)
+  if (length(numeric_vars) != 0) {
+    for (nv in numeric_vars) {
+      # create the factor coefficients:
+      # bvar1_factor1[factor1] + bvar1_factor2[factor2] + ...
+      if (length(factor_vars) == 0) {
+        factor_coefs <- paste0("b", nv)
+      } else {
+        factor_coefs <- paste0("b", nv, "_",
+                               factor_vars, "[", factor_vars, "]",
+                               collapse = " + ")
+        factor_coefs <- paste0("b", nv, " + ", factor_coefs)
+      }
+      factor_coefs <- paste0("(", factor_coefs, ") * ", nv)
+
+      m_slope <- c(m_slope, factor_coefs)
     }
-    factor_coefs <- paste0("(", factor_coefs, ") * ", nv)
-
-    m_slope <- c(m_slope, factor_coefs)
+    m_slope <- paste0(m_slope, collapse = " + ")
   }
-  m_slope <- paste0(m_slope, collapse = " + ")
-
 
   # Build up a string of the intercept terms. Also store a list of terms
   if (fls[["include_intercept"]]) {
@@ -39,7 +40,11 @@
       )
       m_intercept <- paste0("a0 + ", m_intercept)
     }
-    m_lm <- paste0(m_link, " <- ", m_intercept, " + ", m_slope)
+    if (is.null(m_slope)) {
+      m_lm <- paste0(m_link, " <- ", m_intercept)
+    } else {
+      m_lm <- paste0(m_link, " <- ", m_intercept, " + ", m_slope)
+    }
   } else {
     m_lm <- paste0(m_link, " <- ", m_slope)
   }
--- scratch/test_f2flist.R
+++ scratch/test_f2flist.R
@@ -1,4 +1,4 @@
-n <- 150
+n <- 300
 x1 <- runif(n, -1, 1)
 x2 <- runif(n, -1, 1)
 a <- 0
@@ -19,7 +19,8 @@ dat_binom <- data.frame(y = y2, k = size, prop = y2 / size, x1 = x1, x2 = x2,
 
 ###################
 devtools::load_all()
-f <- y|k ~ x1 + x2 + gender + age
+
+f <- y|k ~ age
 (fls <- .extractFromFormula(f))
 vars <- fls[["vars"]]
 LHS <- fls[["LHS"]]
@@ -32,17 +33,24 @@ if (grepl(pattern = "\\|", LHS)) {
 }
 .getDataClasses(fls, dat_binom)
 .getModelTerms(fls, .getDataClasses(fls, dat_binom))
+.buildLinkFormula(fls, .getDataClasses(fls, dat_binom), "logit")
+f2flist(y|k ~ age + gender, dat_binom, "logit")
 f2flist(y|k ~ x1 + x2, dat_binom, "logit")
 f2flist(y|k ~ x1 + x2 + age + gender, dat_binom, "logit")
 
 
+glm(y ~ 0 + age + gender, dat_bern, family = binomial("logit"))
 
-glm(y ~ x1 + x2, dat_bern, family = binomial("logit"))
-
-test_fit <- bayesPF(y|k ~ x1 + x2, dat_binom, "logit",
+library(rstan)
+library(rethinking)
+test_fit <- bayesPF(y|k ~ age, dat_binom, "logit",
                     iter=4500, warmup=2000, chains=4, cores=4)
-precis(test_fit)
+precis(test_fit, depth = 2)
 
-test_fit2 <- bayesPF(y ~ x1 + x2, dat_bern, "logit",
+test_fit2 <- bayesPF(y ~ x1 + x2 + age + gender, dat_bern, "logit",
                      iter=4500, warmup=2000, chains=4, cores=4)
-precis(test_fit2)
+precis(test_fit2, depth = 2)
+
+test_fit3 <- bayesPF(y ~ 0 + x1 + x2, dat_bern, "logit",
+                     iter=4500, warmup=2000, chains=4, cores=4)
+precis(test_fit3)

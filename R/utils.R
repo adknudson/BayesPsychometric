7474048920ce8bb5d8cdc8aeb55063c1169debf8
Alex <aknudson@nevada.unr.edu>
2019-11-11 10:50:56 -0800

    trying to get internal help links to work

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,8 @@
 #' Computes the logistic of a number
 #'
 #' The function is defined by \deqn{\frac{1}{1 + \exp(-x)}}{1 / ( 1 + exp(-x) )}
-#' This function is the inverse of the [logit()] function.
+#' @family logistic equations
+#' @seealso \code{\link{logit}}
 #'
 #' @param x A real number, including ±Inf
 #' @return The logistic of \code{x}, a real number in the range \code{[0, 1]}
@@ -12,8 +13,10 @@ logistic <- function(x) {
 
 #' Computes the log-odds of a number
 #'
-#' The function is defined by \deqn{\text{ln}\left(\frac{p}{1-p}\right)}{ln( p / ( 1 - p ) )}
-#' This function has an inverse, the [logistic()] function.
+#' The function is defined by
+#' \deqn{\text{ln}\left(\frac{p}{1-p}\right)}{ln( p / ( 1 - p ) )}
+#' @family logistic equations
+#' @seealso \code{\link{logistic}}
 #'
 #' @param p A real number in the range \code{[0, 1]}
 #' @return The log-odds of \code{p}, a real number symmetric around 0
